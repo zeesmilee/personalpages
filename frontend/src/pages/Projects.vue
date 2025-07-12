76b97841382faf0d6c5ad8df50d8d63827e30482
zeesmilee <166497582+zeesmilee@users.noreply.github.com>
2025-07-12 16:50:44 +0700

    Chỉnh sửa lần 2

--- frontend/src/pages/Projects.vue
+++ frontend/src/pages/Projects.vue
@@ -1,9 +1,11 @@
 <template>
+  <div>
   <h2>Dự án cá nhân</h2>
   <ul>
     <li>Website bán hàng bằng Vue + Node.js</li>
     <li>Ứng dụng To-do List</li>
   </ul>
+  </div>
 </template>
 
 <script setup>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,14 @@ import Home from '../pages/Home.vue'
 import Projects from '../pages/Projects.vue'
 import Skills from '../pages/Skills.vue'
 import Contact from '../pages/Contact.vue'
+import EditProfile from '../pages/EditProfile.vue'
 
 const routes = [
     { path: '/', component: Home },
     { path: '/projects', component: Projects },
     { path: '/skills', component: Skills },
-    { path: '/contact', component: Contact }
+    { path: '/contact', component: Contact },
+    { path: '/edit-profile', component: EditProfile },
 ]
 
 const router = createRouter({
